f6356b13b7b9785541621d3ca164506bdbdc3825
mmainulhossain <75877320+mmainulhossain@users.noreply.github.com>
2020-12-12 12:05:39 +0200

    Update Schedule.java

--- pratical_project/src/main/java/model/Schedule.java
+++ pratical_project/src/main/java/model/Schedule.java
@@ -1,10 +1,46 @@
 package model;
+import java.util.*;
 
 public class Schedule {
+    private int scheduleId;
+    private Date startTime;
+    private int movieId;
+    private int roomId;
 
     public Schedule()
     {
 
     }
 
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
 }
